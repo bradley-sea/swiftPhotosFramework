e8bd034372b96c7b5c252620f4c5d52ac2be6b37
bradleypj@icloud.com <bradleypj@icloud.com>
2014-08-06 12:11:12 -0700

    added photo editing for filter

--- Week3App/ViewController.swift
+++ Week3App/ViewController.swift
@@ -9,16 +9,24 @@
 import UIKit
 import Photos
 
-class ViewController: UIViewController, SelectedPhotoDelegate {
+class ViewController: UIViewController, SelectedPhotoDelegate, PHPhotoLibraryChangeObserver {
     
     var collectionsFetchResults = [PHFetchResult]()
     var collectionsLocalizedTitles = [String]()
+    
+    var selectedAsset : PHAsset?
+    
+    let adjustmentFormatterIndentifier = "com.filterappdemo.cf"
+    
+    var context = CIContext(options: nil)
                             
 
     
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        PHPhotoLibrary.sharedPhotoLibrary().registerChangeObserver(self)
+        
 //        var smartAlbums = PHAssetCollection.fetchAssetCollectionsWithType(PHAssetCollectionType.SmartAlbum, subtype: PHAssetCollectionSubtype.AlbumRegular, options: nil)
 //        var topLevelUserCollections = PHCollectionList.fetchTopLevelUserCollectionsWithOptions(nil)
 //        self.collectionsFetchResults = [smartAlbums,topLevelUserCollections]
@@ -80,19 +88,102 @@ class ViewController: UIViewController, SelectedPhotoDelegate {
     
     func photoSelected(asset : PHAsset) -> Void {
         
-        var targetSize = CGSize(width: CGRectGetWidth(self.imageView.bounds), height: CGRectGetHeight(self.imageView.bounds))
+        self.selectedAsset = asset
+        self.updateImage()
         
-        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: targetSize, contentMode: PHImageContentMode.AspectFit, options: nil, resultHandler: {(result : UIImage!, [NSObject : AnyObject]!) -> Void in
-            
-            if result {
-                self.imageView.image = result
-            }
-            
-            })
+//        var targetSize = CGSize(width: CGRectGetWidth(self.imageView.bounds), height: CGRectGetHeight(self.imageView.bounds))
+//        
+//        PHImageManager.defaultManager().requestImageForAsset(asset, targetSize: targetSize, contentMode: PHImageContentMode.AspectFit, options: nil, resultHandler: {(result : UIImage!, [NSObject : AnyObject]!) -> Void in
+//            
+//            if result {
+//                self.imageView.image = result
+//            }
+//            
+//            })
 
         
         
         
+    }
+    
+    @IBAction func handleSepiaPressed(sender: AnyObject) {
+        
+        var options = PHContentEditingInputRequestOptions()
+        options.canHandleAdjustmentData = {(data : PHAdjustmentData!) -> Bool in
+            
+            return data.formatIdentifier == self.adjustmentFormatterIndentifier && data.formatVersion == "1.0"
+        }
+        
+        self.selectedAsset!.requestContentEditingInputWithOptions(options, completionHandler: { ( contentEditingInput : PHContentEditingInput!, info : [NSObject : AnyObject]!) -> Void in
+            
+            //grabbing the image and converting it to CIImage
+            var url = contentEditingInput.fullSizeImageURL
+            var orientation = contentEditingInput.fullSizeImageOrientation
+            var inputImage = CIImage(contentsOfURL: url)
+            inputImage = inputImage.imageByApplyingOrientation(orientation)
+            
+            //creating the filter
+            var filterName = "CISepiaTone"
+            var filter = CIFilter(name: filterName)
+            filter.setDefaults()
+            filter.setValue(inputImage, forKey: kCIInputImageKey)
+            var outputImage = filter.outputImage
+            
+            var cgimg = self.context.createCGImage(outputImage, fromRect: outputImage.extent())
+            var finalImage = UIImage(CGImage: cgimg)
+            var jpegData = UIImageJPEGRepresentation(finalImage, 1.0)
+            
+            //create our adjustmentdata
+            var adjustmentData = PHAdjustmentData(formatIdentifier: self.adjustmentFormatterIndentifier, formatVersion: "1.0", data: jpegData)
+            var contentEditingOutput = PHContentEditingOutput(contentEditingInput:contentEditingInput)
+            jpegData.writeToURL(contentEditingOutput.renderedContentURL, atomically: true)
+            contentEditingOutput.adjustmentData = adjustmentData
+            
+            //requesting the change
+            PHPhotoLibrary.sharedPhotoLibrary().performChanges({
+                //change block
+                var request = PHAssetChangeRequest(forAsset: self.selectedAsset)
+                request.contentEditingOutput = contentEditingOutput
+
+                }, completionHandler: { (success : Bool,error : NSError!) -> Void in
+                    //completionHandler for the change
+                    if !success {
+                        println(error.localizedDescription)
+                    }
+                
+            })
+            
+            
+        })
+    }
+    
+    func updateImage() {
+        
+        var targetSize = self.imageView.frame.size
+        PHImageManager.defaultManager().requestImageForAsset(self.selectedAsset, targetSize: targetSize, contentMode: PHImageContentMode.AspectFill, options: nil) { (result : UIImage!, info : [NSObject : AnyObject]!) -> Void in
+            self.imageView.image = result
+        }
+        
+    }
+    
+    func photoLibraryDidChange(changeInstance: PHChange!) {
+        
+        NSOperationQueue.mainQueue().addOperationWithBlock { () -> Void in
+            
+            if self.selectedAsset != nil {
+                var changeDetails = changeInstance.changeDetailsForObject(self.selectedAsset)
+                if changeDetails != nil {
+                    self.selectedAsset = changeDetails.objectAfterChanges as? PHAsset
+                    
+                    if changeDetails.assetContentChanged {
+                        
+                        self.updateImage()
+                    
+                    }
+                    
+                }
+            }
+        }
     }
 }
 
